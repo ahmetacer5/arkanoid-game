2398bdb116f4912ff90cadad15c67431d109eb37
Ahmet Acer <ahmet.acer@dnb.no>
2023-04-18 00:05:59 +0200

    Documenting functions below - isBallTouchingTheFenceSides - isRacketTouchingTheBall

--- src/utils/isBallTouchingTheFenceSides.ts
+++ src/utils/isBallTouchingTheFenceSides.ts
@@ -1,6 +1,15 @@
 import BouncingBall from "../core/BouncingBall";
 import { FenceTouch } from "../types";
 
+/**
+ * Check if the ball is touching any of the sides of a fence.
+ *
+ * @param {number} fenceHeight - The height of the fence in pixels.
+ * @param {number} fenceWidth - The width of the fence in pixels.
+ * @param {BouncingBall} ball - An object representing the bouncing ball.
+ *
+ * @returns {FenceTouch} An object indicating which sides of the fence the ball is touching.
+ */
 function isBallTouchingTheFenceSides(
   fenceHeight: number,
   fenceWidth: number,
@@ -8,18 +17,22 @@ function isBallTouchingTheFenceSides(
 ): FenceTouch {
   const fenceTouch: FenceTouch = {};
 
+  // Check if the ball is touching the right side of the fence
   if (ball.posX + ball.radius >= fenceWidth) {
     fenceTouch.right = true;
   }
 
+  // Check if the ball is touching the left side of the fence
   if (ball.posX - ball.radius <= 0) {
     fenceTouch.left = true;
   }
 
+  // Check if the ball is touching the bottom side of the fence
   if (ball.posY + ball.radius >= fenceHeight) {
     fenceTouch.bottom = true;
   }
 
+  // Check if the ball is touching the top side of the fence
   if (ball.posY - ball.radius <= 0) {
     fenceTouch.top = true;
   }
--- src/utils/isRacketTouchingTheBall.ts
+++ src/utils/isRacketTouchingTheBall.ts
@@ -2,6 +2,15 @@ import BouncingBall from "../core/BouncingBall";
 import Racket from "../core/Racket";
 import PlayerType from "../enums/PlayerType.enum";
 
+/**
+ * Check if a racket is touching a bouncing ball.
+ *
+ * @param {Racket} racket - An object representing the racket.
+ * @param {BouncingBall} ball - An object representing the bouncing ball.
+ *
+ * @returns {boolean} Whether or not the racket is touching the ball.
+ */
+
 function isRacketTouchingTheBall(racket: Racket, ball: BouncingBall) {
   if (racket.player === PlayerType.Left) {
     if (
